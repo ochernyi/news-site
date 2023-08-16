dc52614a8440732489d866752e8e4a59ed867c89
Oleh <ochernyi04@gmail.com>
2023-08-16 22:21:46 +0300

    Format code according PEP8 in models.py

--- users/models.py
+++ users/models.py
@@ -10,12 +10,7 @@ class UserManager(BaseUserManager):
 
     use_in_migrations = True
 
-    def _create_user(
-            self,
-            email: str,
-            password: str,
-            **extra_fields: dict
-    ) -> User:
+    def _create_user(self, email: str, password: str, **extra_fields: dict) -> User:
         """Create and save a User with the given email and password."""
         if not email:
             raise ValueError("The given email must be set")
@@ -26,10 +21,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(
-        self,
-        email: str,
-        password: str = None,
-        **extra_fields: dict
+        self, email: str, password: str = None, **extra_fields: dict
     ) -> User:
         """Create and save a regular User with the given email and password."""
         extra_fields.setdefault("is_staff", False)
@@ -37,10 +29,7 @@ class UserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(
-        self,
-        email: str,
-        password: str,
-        **extra_fields: dict
+        self, email: str, password: str, **extra_fields: dict
     ) -> ValueError | User:
         """Create and save a SuperUser with the given email and password."""
         extra_fields.setdefault("is_staff", True)
