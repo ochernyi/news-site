2c92f7fd3b9721d58eacbcaa12bfaa1dd1e334b2
Oleh <ochernyi04@gmail.com>
2023-08-16 22:28:24 +0300

    Replace generics for viewsets

--- news/views.py
+++ news/views.py
@@ -1,30 +1,64 @@
-from rest_framework import generics, permissions
+from typing import Any
+from urllib.request import Request
+
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import viewsets, permissions, filters, status
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
 from .models import Category, News
 from .serializers import CategorySerializer, NewsSerializer
 from .permissions import IsAuthorOrReadOnly
 
 
-class CategoryList(generics.ListAPIView):
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
-
-class NewsList(generics.ListCreateAPIView):
-    queryset = News.objects.all()
-    serializer_class = NewsSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-
-class NewsDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = News.objects.all()
-    serializer_class = NewsSerializer
-    permission_classes = [IsAuthorOrReadOnly]
-
-
-class NewsCreate(generics.CreateAPIView):
-    queryset = News.objects.all()
-    serializer_class = NewsSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class NewsPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 10
+
+
+class NewsViewSet(viewsets.ModelViewSet):
+    serializer_class = NewsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ["created_at", "updated_at"]
+    search_fields = ["title", "content"]
+    pagination_class = NewsPagination
+
+    def get_queryset(self):
+        queryset = News.objects.all()
+        category_id = self.request.query_params.get("category")
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "category",
+                type=OpenApiTypes.INT,
+                description="Filter news by category id (ex. ?category=2)",
+            ),
+        ]
+    )
+    def list(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        return super().list(request, *args, **kwargs)
